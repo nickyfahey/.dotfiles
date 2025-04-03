2ff99ea5a6d319b66a0214f3ea48df87e02fb5e4
Nicky Fahey <nickyrfahey@gmail.com>
2025-04-03 07:29:30 +0100

    tidy up bash script

--- battery-monitor/bin/batcheck.sh
+++ battery-monitor/bin/batcheck.sh
@@ -1,45 +1,28 @@
 #!/bin/bash
 
-# paplay needs environment variable exported:
-# export XDG_RUNTIME_DIR="/run/user/1000"
+low_noise=/home/nicky/bin/suspend-error.oga
+full_noise=/home/nicky/bin/alert.wav
 
-
-noice=/home/nicky/bin/suspend-error.oga
-fullNoice=/home/nicky/bin/alert.wav
-
-
-checkbat(){
-	batStatus=$(cd /sys/class/power_supply/BAT1 && cat status)
+check_bat(){
+	status=$(cd /sys/class/power_supply/BAT1 && cat status)
 	capacity=$(cd /sys/class/power_supply/BAT1 && cat capacity)
 
 	# Charging
-	# if [[ $batStatus != Discharging ]]; then
+	# if [[ $status != Discharging ]]; then
 		# if (( $capacity >= 98 )); then
-			# pw-play $fullNoice
+			# pw-play $full_noise
 		# fi
+
 	# Discharging
-	if [[ $batStatus == Discharging ]]; then
+	if [[ $status == Discharging ]]; then
 		if (( $capacity <= 5 )); then
-			pw-play $noice
-			# sleep 0.2s
-			# pw-play $noice
-			# sleep 0.2s
-			# pw-play $noice
-		# elif (( $capacity <= 10 )); then
-		# 	pw-play $noice
-		# 	sleep 0.2s
-		# 	pw-play $noice
-		# elif (( $capacity <= 12 )); then
-		# 	pw-play $noice
-		# elif (( $gamemode == 1 && $capacity <= 20 )); then
-		# 	paplay $noice
+			pw-play $low_noise
 		fi
 	fi
 }
 
 while true
 do
-	checkbat
+	check_bat
 	sleep 5
 done
-
